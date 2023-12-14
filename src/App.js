9f95d64498b306ac47908e6dcfc007591fd1c29a
Pedro Ferreira <yeaper@gmail.com>
2023-12-14 00:42:57 +0000

    trello from chatgpt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import './App.css';
 
-function App() {
+const initialData = {
+  tasks: {
+    'a': { id: 'a', content: 'Task 1' },
+    'b': { id: 'b', content: 'Task 2' },
+    'c': { id: 'c', content: 'Task 3' },
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To Do',
+      taskIds: ['a', 'b', 'c'],
+    },
+    'column-2': {
+      id: 'column-2',
+      title: 'In Progress',
+      taskIds: [],
+    },
+  },
+  columnOrder: ['column-1', 'column-2'],
+};
+
+const App = () => {
+  const [data, setData] = useState(initialData);
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const startColumn = data.columns[source.droppableId];
+    const finishColumn = data.columns[destination.droppableId];
+
+    if (startColumn === finishColumn) {
+      const newTaskIds = Array.from(startColumn.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...startColumn,
+        taskIds: newTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setData(newData);
+    } else {
+      const startTaskIds = Array.from(startColumn.taskIds);
+      startTaskIds.splice(source.index, 1);
+      const newStartColumn = {
+        ...startColumn,
+        taskIds: startTaskIds,
+      };
+
+      const finishTaskIds = Array.from(finishColumn.taskIds);
+      finishTaskIds.splice(destination.index, 0, draggableId);
+      const newFinishColumn = {
+        ...finishColumn,
+        taskIds: finishTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newStartColumn.id]: newStartColumn,
+          [newFinishColumn.id]: newFinishColumn,
+        },
+      };
+
+      setData(newData);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      {data.columnOrder.map((columnId) => {
+        const column = data.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+
+        return (
+          <div key={column.id} className="column">
+            <h2>{column.title}</h2>
+            <Droppable droppableId={column.id}>
+              {(provided) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className="task-list"
+                >
+                  {tasks.map((task, index) => (
+                    <Draggable
+                      key={task.id}
+                      draggableId={task.id.toString()}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className="task"
+                        >
+                          {task.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </div>
+        );
+      })}
+    </DragDropContext>
   );
-}
+};
 
 export default App;
